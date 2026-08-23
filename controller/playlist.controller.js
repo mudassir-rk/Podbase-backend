ea956d96bab053f92b00b2fc53cf1ce56ee16ce9
mudassir-rk <mudassir@example.com>
2026-08-23 09:13:12 +0530

    add different methods of playlist controllers

--- controller/playlist.controller.js
+++ controller/playlist.controller.js
@@ -158,6 +158,8 @@ const deletePlaylist = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, delplaylist, "Playlist deleted successfully"))
 })
 
+
+
 // const updatePlaylist = asyncHandler(async (req, res) => {
 //     
 //     const {playlistId} = req.params
