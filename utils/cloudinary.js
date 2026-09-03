28b739fd9fcd782dc2973eecd000dc90e388ec7a
mudassir-rk <mudassir.mk0106@gmail.com>
2026-09-03 18:51:32 +0530

    Refactor cloudinary configuration to use environment variables and clean up commented code

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,33 +1,12 @@
 import { v2 as cloudinary } from 'cloudinary';
+import dotenv from "dotenv";
+dotenv.config()
 import fs from 'fs';
-    // Configuration
     cloudinary.config({ 
         cloud_name: 'sqolui98', 
-        api_key: '455241831641233', 
-        api_secret: 'pwtT-vrsgC3roYG-kUFbF1CrRAE' // 
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
     });
-    // Upload an image
-//      const uploadOnCloudinary = async (localFilePath) =>{
-//         try{
-//             if(!localFilePath){
-               
-//                 throw new Error('No file path provided');
-//            }
-//            const response = await cloudinary.uploader.upload(localFilePath,
-//             {
-//                 response_type: "auto"
-//     })
-//     console.log("file is uploaded successfully" , response.url);
-//     fs.unlinkSync(localFilePath)
-//     return response;
-// }
-//        // to remove the locally saved  temporary file as the upload 
-//        catch(error)  {
-//           fs.unlinkSync(localFilePath)
-//           return null;
-//        };
-//     console.log(uploadResult);
-//      }
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) {
